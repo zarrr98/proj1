e7ff034e2bc212c4d3916f4c8b2c7ed90096a969
zahra <sabah.z3939@gmail.com>
2020-09-08 12:41:23 +0430

    make some changes in for deployment stuff

--- proj1/src/utils/configs.js
+++ proj1/src/utils/configs.js
@@ -1,8 +1,8 @@
 export const URL = {
-    protocol: "http",
-    baseURL: "localhost",
-    port: "4000/apis",
-    // protocol: "https",
-    // baseURL: "transis.herokuapp.com",
-    // port: "/apis",
+    // protocol: "http",
+    // baseURL: "localhost",
+    // port: "3000/apis",
+    protocol: "https",
+    baseURL: "transis.herokuapp.com",
+    port: "/apis",
   };
